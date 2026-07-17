5e0b9259e2b937e4d5c82a85afe3d7ca95b40b4a
unisom0rphic <117302827+unisom0rphic@users.noreply.github.com>
2026-07-18 02:07:31 +0500

    tcp connection executor pipeline integration

--- src/conn.rs
+++ src/conn.rs
@@ -1,4 +1,9 @@
+use std::str::from_utf8_unchecked;
+
+use tokio::sync::mpsc;
+
 use crate::parser::Executor;
+use crate::storage::KvStore;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::sync::oneshot;
@@ -16,37 +21,19 @@ pub struct StoreRequest {
     pub tx: oneshot::Sender<Vec<u8>>,
 }
 
-/*
-The PLAN:
-basically rn we can parse data.
-We need to provide an api for the executor.
-
-the pipeline:
-open a connection
-parse command
-if ok(command) => execute
-else if err(command) => print err
-
-execute:
-create a StoreRequest
-send it to the mpsc channel
-receive data back
-print it
-
-it would be nice to have an interface like:
-let result = ???(cmd);
-without explicity creating channels
-
-- so we need a function to create a mpsc, bind rx to exec and return tx (1)
-- automatic oneshot creation
-*/
-
 pub async fn open_connection() {
     let listener = TcpListener::bind("0.0.0.0:6767").await.unwrap();
     println!("Server listening on 0.0.0.0:6767");
 
-    if let Ok((mut stream, addr)) = listener.accept().await {
+    // disconnects after the first executed command
+    while let Ok((mut stream, addr)) = listener.accept().await {
         println!("✅ Connection accepted from {}", addr);
+
+        const CHANNEL_SIZE: usize = 10;
+        let (tx, rx) = mpsc::channel(CHANNEL_SIZE);
+        let mut exec = Executor::new(KvStore::new(), rx);
+
+        tokio::spawn(async move { exec.run().await });
 
         let _ = tokio::spawn(async move {
             let mut buffer = [0; 1024];
@@ -55,9 +42,44 @@ pub async fn open_connection() {
                 Ok(Ok(0)) => println!("Client disconnected without sending data"),
                 Ok(Ok(n)) => {
                     println!("📥 Received {} bytes: {:?}", n, &buffer[..n]);
+
                     let parsed_data = Executor::parse(std::str::from_utf8(&buffer[..n]).unwrap());
+
+                    // TODO:
+                    // shouldn't fail if data is invalid
+                    // send the message to the client instead
+                    let parsed_cmd = match parsed_data {
+                        Ok(cmd) => cmd,
+                        Err(e) => {
+                            println!("Error parsing the data: {}", e);
+                            return;
+                        }
+                    };
+                    println!("Parsed command: {:?}", parsed_cmd);
+
+                    let (otx, orx) = oneshot::channel();
+
+                    let request = StoreRequest {
+                        cmd: parsed_cmd,
+                        tx: otx,
+                    };
+
+                    tx.send(request)
+                        .await
+                        .expect("Failed to send data: channel closed");
+                    println!("Send request");
+
+                    let server_reply = match orx.await {
+                        Ok(d) => String::from_utf8(d),
+                        Err(e) => {
+                            println!("Error receiving the data: {}", e);
+                            return;
+                        }
+                    };
+                    println!("Received a response: {:?}", server_reply);
+
                     let _ = stream
-                        .write_all(format!("{:?}", parsed_data.unwrap()).as_bytes())
+                        .write_all(format!("{:?}", server_reply).as_bytes())
                         .await;
                 }
                 Ok(Err(e)) => eprintln!("Read error: {}", e),
